1dc9c1a796a3a2c96e6a9d3d86df32900bea51c1
BojanaMarojevic <bojana.marojevikj@finki.ukim.mk>
2025-02-09 13:14:36 +0100

    Initial commit

---
+++ src/test/java/mk/ukim/finki/wp/fcseservices/FcseServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wp.fcseservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcseServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
